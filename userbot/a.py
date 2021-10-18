b4607a53129e98a09e7da78b0b800472c248095c
whitehatjrchintu <74552895+whitehatjrchintu@users.noreply.github.com>
2021-10-18 18:51:31 +0530

    Add files via upload

--- userbot/a.py
+++ userbot/a.py
@@ -1,1 +1,7 @@
+from pyrogram import Client
+from os import environ
 
+api_id = int(environ["API_ID"])
+api_hash = environ["API_HASH"]
+bot_token = environ["BOT_TOKEN"]
+app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
